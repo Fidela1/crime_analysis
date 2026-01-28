7c69fd5e0727ee49bb233f9686b82bbad2bb320f
Fidela1 <fidelatuyizere4@gmail.com>
2026-01-28 19:22:54 +0200

    removed unnecessary comments

--- task4.py
+++ task4.py
@@ -6,7 +6,7 @@ df = pd.read_csv("SouthAfricaCrimeStats_v2.csv")
 # Select the year for analysis
 year = "2015-2016"
 
-# Group by province and sum crimes for the selected year
+
 province_crimes = df.groupby("Province")[year].sum()
 
 print("Total crimes per province in", year)
